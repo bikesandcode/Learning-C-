619f671356ec15f5db073e21177885e31e49ca6f
Warren Taylor <wtaylor@pcis.com>
2011-11-21 11:29:16 -0800

    Removed sample programs from me and others.

--- BrainfuckInterpreter/Main.cs
+++ BrainfuckInterpreter/Main.cs
@@ -4,10 +4,10 @@ namespace BrainfuckInterpreter
 {
 	class MainClass
 	{
-		private int[] buffer = new int[30000];
+		private byte[] buffer = new byte[30000];
 		private int dataPointer = 0;
+		private int maxDataPointerSet = 0;
 		private int codePointer = -1;
-		private int nestedLoopDepth = 0;
 		
 		private string program;
 		private char[] codeArray;
@@ -18,86 +18,146 @@ namespace BrainfuckInterpreter
 			this.codeArray = program.ToCharArray();
 		}
 		
+		/**
+		 * Increments value under the data pointer
+		 **/
+		private void increment()
+		{
+			if( dataPointer > maxDataPointerSet ) maxDataPointerSet = dataPointer;
+			buffer[dataPointer]++;	
+		}
+		
+		/**
+		 * Increments value under the data pointer
+		 **/
+		private void decrement(){ buffer[dataPointer]--; }
+		private int bufferValue(){ return buffer[dataPointer];}
+		
+		private void incrementCodePointer(){ codePointer++; }
+		private void decrementCodePointer(){ 
+			codePointer--; 
+			if( codePointer < 0 ){
+				Console.WriteLine("Code Pointer has gone < 0 at codePoint {0}", codePointer);
+				WriteMemory();
+			}
+		}
+		private char codeValue(){ return codeArray[codePointer]; }
+		
+		private void incrementDataPointer(){ dataPointer++; }
+		private void decrementDataPointer(){ 			
+			dataPointer--; 
+			if( dataPointer < 0 ){
+				Console.WriteLine("Data Pointer has gone < 0 at codePoint {0}", codePointer);
+				WriteMemory();
+			}
+		}
+		
+		private string padInt( int number )
+		{
+			if( number == 0 ) return "000";
+			if( number < 10 ) return "00" + number.ToString();
+			if( number < 100 ) return "0" + number.ToString();
+			return number.ToString();
+		}
+		
+		/**
+		 * Console.WriteLines a snapshot of memory, pointer locations, etc.
+		 **/
+		public void WriteMemory()
+		{
+			///*
+			Console.WriteLine("DataPtr: {0}", dataPointer);
+			Console.WriteLine("CodePtr: {0}", codePointer);			
+			
+			var output = "";
+			for( var i = 0; i <= maxDataPointerSet; i++ )
+			{
+				if( i % 20 == 0 ){
+					Console.WriteLine(output);
+					output = "";
+				}
+				output = output + padInt(buffer[i]) + " ";
+			}			
+			Console.WriteLine(output);
+			Console.WriteLine("*********************");
+			//*/
+		}
 		
 		public void Execute()
 		{
-			try{
-				while( codePointer < program.Length-1 )
-				{
-					codePointer++;
-					char next = codeArray[codePointer];
-					switch (next)
-					{
-						case '+': 
-						{ 
-							//Console.WriteLine("++");
-							buffer[dataPointer]++; 
-							break; 
-						}
-						case '-': 
-						{ 
-							//Console.WriteLine("--");
-							buffer[dataPointer]--; 
-							break;
-						}
-						case '.': 
-						{ 
-							//Console.WriteLine("Print ptr={0}", dataPointer);
-							Console.Write((char)(buffer[dataPointer])); 
-							break;
-						}
-						case ',': 
-						{
-							int read = Console.Read();
-							buffer[dataPointer] = read;
-							break;
-						}
-						case '[': 
-						{ 
-							//Console.WriteLine("{");
-							LoopStart(); 
-							break;
-						}
-						case ']': 
-						{ 
-							//Console.WriteLine("}");
-							LoopEnd(); 
-							break;
-						}
-						case '<': 
-						{ 						
-							//Console.WriteLine("ptr--");
-							dataPointer--; 
-							break;
-						}
-						case '>': 
-						{
-							//Console.WriteLine("ptr++");
-							dataPointer++; 
-							break;
-						}
-						default: {break;} //Ignore all else
-					}				
-				}
-			}
-			catch(System.IndexOutOfRangeException e)
+			while( codePointer < program.Length-1 )
 			{
-				Console.WriteLine("Array index out of range hit. Dump: ");
-				Console.WriteLine("DataPtr: {0}", dataPointer);
-				Console.WriteLine("CodePtr: {0}", codePointer);
+				incrementCodePointer();
+				char next = codeValue();
+				switch (next)
+				{
+					case '+': 
+					{ 
+						increment();
+						break; 
+					}
+					case '-': 
+					{ 
+						decrement(); 
+						break;
+					}
+					case '.': 
+					{ 
+						Console.Write((char)(buffer[dataPointer])); 
+						break;
+					}
+					case ',': 
+					{
+						int read = Console.Read();
+						buffer[dataPointer] = (byte)read;
+						break;
+					}
+					case '[': 
+					{ 
+						LoopStart(); 
+						break;
+					}
+					case ']': 
+					{ 
+						LoopEnd(); 
+						break;
+					}
+					case '<': 
+					{ 						
+						decrementDataPointer();
+						break;
+					}
+					case '>': 
+					{
+						incrementDataPointer();
+						break;
+					}
+					default: {break;} //Ignore all else
+				}				
 			}
+
 		}
 		
 		/**
 		 * Called when a [ character is hit, loop start.
-		 * If the data pointer is currently -> 0, jump to
-		 * just past the next ].
 		 * Otherwise, truck forward.
 		 **/
 		private void LoopStart()
 		{
-			nestedLoopDepth++;
-			if( buffer[dataPointer] == 0 ) ScanToMatchingClose(false);
+			if( bufferValue() == 0 ){
+				ScanToLoopEnd(); 				
+			}
+			
+		}
+		
+		private void ScanToLoopEnd()
+		{
+			incrementCodePointer();
+			while(codeValue() != ']')
+			{				
+				if( codeValue() == '[' ) ScanToLoopEnd();
+				incrementCodePointer();
+			}			
 		}
 		
 		/**
@@ -106,33 +166,17 @@ namespace BrainfuckInterpreter
 		 **/
 		private void LoopEnd()
 		{
-			ScanToMatchingOpen(false);
-			//And back so the next tick puts us on the loop start.
-			codePointer--;
-			nestedLoopDepth--;
+			if( bufferValue() != 0 ) ScanToLoopStart();
 		}
 		
-		/**
-		 * Scan forward to the matching close loop character
-		 **/
-		private void ScanToMatchingClose(bool nested)
+		private void ScanToLoopStart()
 		{
-			while( codeArray[codePointer] != ']' )
-			{
-				codePointer++;
-				if( codeArray[codePointer] == '[' ) ScanToMatchingClose(true);
-			}
-			if( nested ) codePointer++;
-		}
-		
-		private void ScanToMatchingOpen(bool nested)
-		{
-			while( codeArray[codePointer] != '[' )
-			{
-				codePointer--;
-				if( codeArray[codePointer] == ']' ) ScanToMatchingOpen(true);
-			}
-			if( nested ) codePointer--;
+			decrementCodePointer();
+			while( codeValue() != '[')
+			{				
+				if( codeValue() == ']' ) ScanToLoopStart(); 
+				decrementCodePointer();
+			}			
 		}
 		
 		public static void Main (string[] args)
@@ -144,6 +188,7 @@ namespace BrainfuckInterpreter
 			
 			var bf = new MainClass(contents);
 			bf.Execute();
+			//bf.WriteMemory();
 			Console.WriteLine("\nExecution finished");
 		}
 	}
